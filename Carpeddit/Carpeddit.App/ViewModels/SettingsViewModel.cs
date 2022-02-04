462a1f7489124228bc38c723b47080c3770df4f0
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-02-04 14:19:54 +0200

    Add converters, view models and more stuff

---
+++ Carpeddit/Carpeddit.App/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Carpeddit.App.ViewModels
+{
+    public class SettingsViewModel : SettingsManager
+    {
+        public SettingsViewModel() { }
+
+        #region Appearance
+        public bool ShowAccountBtnInTitleBar
+        {
+            get => Get("Appearance", nameof(ShowAccountBtnInTitleBar), true);
+            set => Set("Appearance", nameof(ShowAccountBtnInTitleBar), value);
+        }
+        #endregion
+    }
+}
