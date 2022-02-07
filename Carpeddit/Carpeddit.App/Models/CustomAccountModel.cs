c21ddd7cf342969327cf622ea52a0ed4e42b64e2
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-02-07 14:07:29 +0200

    Add things

---
+++ Carpeddit/Carpeddit.App/Models/CustomAccountModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpeddit.App.Models
+{
+    public class CustomAccountModel
+    {
+        public string Scope { get; set; }
+        public string RefreshToken { get; set; }
+        public string AccessToken { get; set; }
+        public int? TokenExpiresIn { get; set; }
+        public bool LoggedIn { get; set; }
+        public string DeviceId { get; set; }
+    }
+}
---
+++ Carpeddit/Carpeddit.App/Other/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpeddit.App.Other
+{
+    public static class Constants
+    {
+        public static string ClientId = "Fc8XmFLpl2Nd5h4Rcd0sDg";
+        public static string ClientSecret = "B9S_kRLuAie9ToOFLntxIBuLDj-SIA";
+        public static string RedirectUri = "http://127.0.0.1:3000/reddit-callback";
+    }
+}
