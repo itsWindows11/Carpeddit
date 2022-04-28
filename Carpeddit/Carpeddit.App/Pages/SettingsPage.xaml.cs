34488c57ea74bc35e0fd58564e9f1acb161b5c4d
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-04-28 19:59:20 +0200

    Show version in settings

--- Carpeddit/Carpeddit.App/Pages/SettingsPage.xaml.cs
+++ Carpeddit/Carpeddit.App/Pages/SettingsPage.xaml.cs
@@ -1,30 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Windows.ApplicationModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Carpeddit.App.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SettingsPage : Page
     {
         public SettingsPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            Loaded += SettingsPageLoaded;
+        }
+
+        private void SettingsPageLoaded(object sender, RoutedEventArgs e)
+        {
+            VersionTextBlock.Text = $"Carpeddit, version ${string.Format("{0}.{1}.{2}.{3}", Package.Current.Id.Version.Major, Package.Current.Id.Version.Minor, Package.Current.Id.Version.Build, Package.Current.Id.Version.Revision)}";
         }
     }
 }
