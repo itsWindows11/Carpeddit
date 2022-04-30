98b7a58f5a8bc7e11148c08ac3eb27cb3e9f2d5c
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-04-30 15:28:25 +0200

    Fix compile errors

--- Carpeddit/Carpeddit.App/Pages/SettingsPage.xaml.cs
+++ Carpeddit/Carpeddit.App/Pages/SettingsPage.xaml.cs
@@ -1,14 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.ApplicationModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 
 namespace Carpeddit.App.Pages
 {
