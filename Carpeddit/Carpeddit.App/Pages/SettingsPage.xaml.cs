7901c4953716bd05719226181e92c5fb8c0dd3b0
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-04-30 15:24:34 +0200

    Initial window tint implementation

--- Carpeddit/Carpeddit.App/Pages/SettingsPage.xaml.cs
+++ Carpeddit/Carpeddit.App/Pages/SettingsPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,33 @@ namespace Carpeddit.App.Pages
     /// </summary>
     public sealed partial class SettingsPage : Page
     {
+
         public SettingsPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+
+            if (App.SViewModel.ColorMode == 2)
+            {
+                GridViewColorList.SelectedIndex = App.SViewModel.TintColor;
+            }
+        }
+
+        private void Border_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            // Set color mode to "Custom color" for consistency.
+            App.SViewModel.ColorMode = 2;
+            var color = ((sender as Border).Background as SolidColorBrush).Color;
+            GridViewColorList.SelectedIndex = App.SViewModel.TintColorsList.IndexOf(color);
+            App.SViewModel.TintColor = App.SViewModel.TintColorsList.IndexOf(color);
+        }
+
+        private void GridViewColorList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Set color mode to "Custom color" for consistency.
+            App.SViewModel.ColorMode = 2;
+            var color = (Color)e.AddedItems[0];
+            GridViewColorList.SelectedIndex = App.SViewModel.TintColorsList.IndexOf(color);
+            App.SViewModel.TintColor = App.SViewModel.TintColorsList.IndexOf(color);
         }
     }
 }
--- Carpeddit/Carpeddit.App/ViewModels/SettingsViewModel.cs
+++ Carpeddit/Carpeddit.App/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,9 @@
-﻿namespace Carpeddit.App.ViewModels
+﻿using System.Collections.Generic;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Carpeddit.App.ViewModels
 {
     public class SettingsViewModel : SettingsManager
     {
@@ -10,6 +15,77 @@
             get => Get("Appearance", nameof(ShowAccountBtnInTitleBar), true);
             set => Set("Appearance", nameof(ShowAccountBtnInTitleBar), value);
         }
+
+        /// <summary>
+        /// A variable that has a value ranging from 0 to 3.
+        /// Specifies the color mode that the app uses.
+        /// </summary>
+        public int ColorMode
+        {
+            get => Get("Appearance", nameof(ColorMode), 0);
+            set => Set("Appearance", nameof(ColorMode), value);
+        }
+
+        /// <summary>
+        /// The index of the color used for tinting the app background.
+        /// </summary>
+        public int TintColor
+        {
+            get => Get("Appearance", nameof(TintColor), 0);
+            set => Set("Appearance", nameof(TintColor), value);
+        }
+
+        public List<Color> TintColorsList { get; } = new() 
+        {
+            Color.FromArgb(255, 255, 185, 0),
+            Color.FromArgb(255, 255, 140, 0),
+            Color.FromArgb(255, 247, 99, 12),
+            Color.FromArgb(255, 202, 80, 16),
+            Color.FromArgb(255, 218, 59, 1),
+            Color.FromArgb(255, 239, 105, 80),
+            Color.FromArgb(255, 209, 52, 56),
+            Color.FromArgb(255, 255, 67, 67),
+            Color.FromArgb(255, 231, 72, 86),
+            Color.FromArgb(255, 232, 17, 35),
+            Color.FromArgb(255, 234, 0, 94),
+            Color.FromArgb(255, 195, 0, 82),
+            Color.FromArgb(255, 227, 0, 140),
+            Color.FromArgb(255, 191, 0, 119),
+            Color.FromArgb(255, 194, 57, 179),
+            Color.FromArgb(255, 154, 0, 137),
+            Color.FromArgb(255, 0, 120, 212),
+            Color.FromArgb(255, 0, 99, 177),
+            Color.FromArgb(255, 142, 140, 216),
+            Color.FromArgb(255, 107, 105, 214),
+            Color.FromArgb(255, 135, 100, 184),
+            Color.FromArgb(255, 116, 77, 169),
+            Color.FromArgb(255, 177, 70, 194),
+            Color.FromArgb(255, 136, 23, 152),
+            Color.FromArgb(255, 0, 153, 188),
+            Color.FromArgb(255, 45, 125, 154),
+            Color.FromArgb(255, 0, 183, 195),
+            Color.FromArgb(255, 3, 131, 135),
+            Color.FromArgb(255, 0, 178, 148),
+            Color.FromArgb(255, 1, 133, 116),
+            Color.FromArgb(255, 0, 204, 106),
+            Color.FromArgb(255, 16, 137, 62),
+            Color.FromArgb(255, 122, 117, 116),
+            Color.FromArgb(255, 93, 90, 88),
+            Color.FromArgb(255, 104, 118, 138),
+            Color.FromArgb(255, 81, 92, 107),
+            Color.FromArgb(255, 86, 124, 115),
+            Color.FromArgb(255, 72, 104, 96),
+            Color.FromArgb(255, 73, 130, 5),
+            Color.FromArgb(255, 16, 124, 16),
+            Color.FromArgb(255, 118, 118, 118),
+            Color.FromArgb(255, 76, 74, 72),
+            Color.FromArgb(255, 105, 121, 126),
+            Color.FromArgb(255, 74, 84, 89),
+            Color.FromArgb(255, 100, 124, 100),
+            Color.FromArgb(255, 82, 94, 84),
+            Color.FromArgb(255, 132, 117, 69),
+            Color.FromArgb(255, 126, 115, 95)
+        };
         #endregion
     }
 }
