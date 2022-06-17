ccb0dfaf599001814e394f760d9ffb733c8bc7ae
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-06-17 13:56:56 +0200

    Update post template in post details

--- Carpeddit/Carpeddit.App/Pages/PostDetailsPage.xaml.cs
+++ Carpeddit/Carpeddit.App/Pages/PostDetailsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BracketPipe;
 using Carpeddit.App.Collections;
 using Carpeddit.App.Models;
+using Microsoft.Toolkit.Uwp.UI.Controls;
 using Reddit.Controllers;
 using RtfPipe;
 using System;
@@ -9,9 +10,11 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI;
 using Windows.UI.Text;
 using Windows.UI.ViewManagement;
@@ -106,46 +109,6 @@ namespace Carpeddit.App.Pages
             return w.ToString();
         }
 
-        private async void UpvoteButton_Click(object sender, RoutedEventArgs e)
-        {
-            ToggleButton toggle = sender as ToggleButton;
-            CommentViewModel comment = (e.OriginalSource as FrameworkElement).DataContext as CommentViewModel;
-
-            if (toggle.IsChecked ?? false)
-            {
-                await comment.OriginalComment.UpvoteAsync();
-                comment.Upvoted = true;
-                comment.Downvoted = false;
-                comment.RawVoteRatio += 1;
-            }
-            else
-            {
-                await comment.OriginalComment.UnvoteAsync();
-                comment.Upvoted = false;
-                comment.Downvoted = false;
-            }
-        }
-
-        private async void DownvoteButton_Click(object sender, RoutedEventArgs e)
-        {
-            ToggleButton toggle = sender as ToggleButton;
-            CommentViewModel comment = (e.OriginalSource as FrameworkElement).DataContext as CommentViewModel;
-
-            if (toggle.IsChecked ?? false)
-            {
-                await comment.OriginalComment.DownvoteAsync();
-                comment.Upvoted = false;
-                comment.Downvoted = true;
-                comment.RawVoteRatio -= 1;
-            }
-            else
-            {
-                await comment.OriginalComment.UnvoteAsync();
-                comment.Upvoted = false;
-                comment.Downvoted = false;
-            }
-        }
-
         private async void RemoveCommentButton_Click(object sender, RoutedEventArgs e)
             => await ((e.OriginalSource as FrameworkElement).DataContext as CommentViewModel).OriginalComment.RemoveAsync();
 
@@ -162,6 +125,193 @@ namespace Carpeddit.App.Pages
             } else
             {
                 comment.ParentComment.Replies.Remove(comment);
+            }
+        }
+
+        private async void MarkdownLinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            if (Uri.TryCreate(e.Link, UriKind.Absolute, out Uri link))
+            {
+                _ = await Launcher.LaunchUriAsync(link);
+                return;
+            }
+
+            if (e.Link.StartsWith("/r/"))
+            {
+                (Window.Current.Content as Frame).Navigate(typeof(SubredditPage), App.RedditClient.Subreddit(name: e.Link.Substring(3)).About());
+                return;
+            }
+
+            if (e.Link.StartsWith("r/"))
+            {
+                (Window.Current.Content as Frame).Navigate(typeof(SubredditPage), App.RedditClient.Subreddit(name: e.Link.Substring(2)).About());
+                return;
+            }
+
+            if (e.Link.StartsWith("/u/"))
+            {
+                MainPage.Current.ContentFrame.Navigate(typeof(YourProfilePage), App.RedditClient.User(name: e.Link.Substring(3)).About());
+                return;
+            }
+
+            if (e.Link.StartsWith("u/"))
+            {
+                MainPage.Current.ContentFrame.Navigate(typeof(YourProfilePage), App.RedditClient.User(name: e.Link.Substring(2)).About());
+                return;
+            }
+        }
+
+        private async void UpvoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton toggle = sender as ToggleButton;
+            PostViewModel post = toggle.Tag as PostViewModel;
+
+            if (toggle.IsChecked ?? false)
+            {
+                await post.Post.UpvoteAsync();
+                post.Upvoted = true;
+                post.Downvoted = false;
+            }
+            else
+            {
+                await post.Post.UnvoteAsync();
+                post.Upvoted = false;
+                post.Downvoted = false;
+            }
+        }
+
+        private async void DownvoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton toggle = sender as ToggleButton;
+            PostViewModel post = toggle.Tag as PostViewModel;
+
+            if (toggle.IsChecked ?? false)
+            {
+                await post.Post.DownvoteAsync();
+                post.Upvoted = false;
+                post.Downvoted = true;
+            }
+            else
+            {
+                await post.Post.UnvoteAsync();
+                post.Upvoted = false;
+                post.Downvoted = false;
+            }
+        }
+
+        private void Title_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            if (Window.Current.Content is Frame rootFrame && sender is TextBlock text && text.Tag is PostViewModel post)
+            {
+                rootFrame.Navigate(typeof(PostDetailsPage), post);
+            }
+        }
+
+        private void UserHyperlink_Click(Windows.UI.Xaml.Documents.Hyperlink sender, Windows.UI.Xaml.Documents.HyperlinkClickEventArgs args)
+        {
+            string text = (sender.Inlines[1] as Windows.UI.Xaml.Documents.Run).Text;
+            if (!text.Contains("[deleted]") && (Window.Current.Content as Frame).CanGoBack)
+            {
+                (Window.Current.Content as Frame).GoBack();
+                MainPage.Current.ContentFrame.Navigate(typeof(YourProfilePage), App.RedditClient.User(text).About());
+            }
+        }
+
+        private void SubredditHyperlink_Click(Windows.UI.Xaml.Documents.Hyperlink sender, Windows.UI.Xaml.Documents.HyperlinkClickEventArgs args)
+        {
+            string text = (sender.Inlines[1] as Windows.UI.Xaml.Documents.Run).Text;
+            if (Window.Current.Content is Frame rootFrame)
+            {
+                rootFrame.Navigate(typeof(SubredditPage), App.RedditClient.Subreddit(text).About());
+            }
+        }
+
+        private async void RemovePostButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as FrameworkElement).DataContext is PostViewModel post)
+            {
+                await post.Post.RemoveAsync();
+
+                (sender as HyperlinkButton).Content = "Removed";
+                (sender as HyperlinkButton).IsEnabled = false;
+
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[5] as HyperlinkButton).Content = "Approve";
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[5] as HyperlinkButton).IsEnabled = true;
+
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[6] as HyperlinkButton).Content = "Spam";
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[6] as HyperlinkButton).IsEnabled = true;
+            }
+        }
+
+        private async void RemoveUserPostButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as FrameworkElement).DataContext is PostViewModel post)
+            {
+                await post.Post.DeleteAsync();
+
+                (sender as HyperlinkButton).Content = "Deleted";
+                (sender as HyperlinkButton).IsEnabled = false;
+            }
+        }
+
+        private void ApproveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as FrameworkElement).DataContext is PostViewModel post)
+            {
+                post.Post.Approve();
+
+                (sender as HyperlinkButton).Content = "Approved";
+                (sender as HyperlinkButton).IsEnabled = false;
+
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[4] as HyperlinkButton).Content = "Remove";
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[4] as HyperlinkButton).IsEnabled = true;
+
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[6] as HyperlinkButton).Content = "Spam";
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[6] as HyperlinkButton).IsEnabled = true;
+            }
+        }
+
+        private async void SpamButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as FrameworkElement).DataContext is PostViewModel post)
+            {
+                await post.Post.RemoveAsync(true);
+
+                (sender as HyperlinkButton).Content = "Spammed";
+                (sender as HyperlinkButton).IsEnabled = false;
+
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[4] as HyperlinkButton).Content = "Remove";
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[4] as HyperlinkButton).IsEnabled = true;
+
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[5] as HyperlinkButton).Content = "Approve";
+                (((sender as HyperlinkButton).Parent as StackPanel).Children[5] as HyperlinkButton).IsEnabled = true;
+            }
+        }
+
+        private async void PinButton_Click(object sender, RoutedEventArgs e)
+        {
+            if ((sender as FrameworkElement).DataContext is PostViewModel post)
+            {
+                // Retrieve first 3 posts of the subreddit, the first 2 are pinned (if not then there's at least one empty slot).
+                var pinned = await Task.Run(() => App.RedditClient.Subreddit(name: post.Post.Subreddit).Posts.GetHot(limit: 3));
+                int indexToInsert = 1;
+
+                if (!pinned[0].Listing.Stickied || (pinned[0].Listing.Stickied && pinned[1].Listing.Stickied))
+                {
+                    indexToInsert = 1;
+                }
+                else if (!pinned[1].Listing.Stickied || pinned[0].Listing.Stickied)
+                {
+                    indexToInsert = 2;
+                }
+
+                // Sticky the post, finally.
+                // This function has misleading documentation and I have no idea how to fix it, so the above
+                // if statements are used here as a workaround.
+                await post.Post.SetSubredditStickyAsync(indexToInsert, false);
+
+                (sender as HyperlinkButton).Content = "Pinned";
+                (sender as HyperlinkButton).IsEnabled = false;
             }
         }
     }
