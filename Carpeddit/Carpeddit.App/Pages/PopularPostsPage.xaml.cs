5841b823f6df47a5dd1b8aed4f5b6afff8d5ed99
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-06-22 13:29:41 +0200

    Port master/detail to popular

--- Carpeddit/Carpeddit.App/Pages/PopularPostsPage.xaml.cs
+++ Carpeddit/Carpeddit.App/Pages/PopularPostsPage.xaml.cs
@@ -8,10 +8,14 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -93,6 +97,52 @@ namespace Carpeddit.App.Pages
             }
 
             return postViews;
+        }
+
+        private void MainList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                SecondPageFrame.Navigate(typeof(PostDetailsPage), (e.AddedItems[0] as PostViewModel, false), new Windows.UI.Xaml.Media.Animation.SuppressNavigationTransitionInfo());
+            }
+            catch (COMException)
+            {
+
+            }
+
+            var appViewTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            appViewTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            appViewTitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            UpdateTitleBarLayout(coreTitleBar);
+
+            Window.Current.SetTitleBar(MainPage.Current.AppTitleBar);
+
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            UpdateTitleBarLayout(sender);
+        }
+
+        private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            MainPage.Current.AppTitleBar.Visibility = sender.IsVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void UpdateTitleBarLayout(CoreApplicationViewTitleBar coreTitleBar)
+        {
+            // Update title bar control size as needed to account for system size changes.
+            MainPage.Current.AppTitleBar.Height = coreTitleBar.Height;
+
+            // Ensure the custom title bar does not overlap window caption controls
+            Thickness currMargin = MainPage.Current.AppTitleBar.Margin;
+            MainPage.Current.AppTitleBar.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
         }
     }
 }
