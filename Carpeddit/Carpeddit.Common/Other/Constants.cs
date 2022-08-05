4826d9175a1c9b2595e4568fc157733cd13e392e
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-08-05 16:17:44 +0200

    Remove test client info

--- Carpeddit/Carpeddit.Common/Other/Constants.cs
+++ Carpeddit/Carpeddit.Common/Other/Constants.cs
@@ -2,8 +2,8 @@
 {
     public static class Constants
     {
-        public static string ClientId = "Fc8XmFLpl2Nd5h4Rcd0sDg";
-        public static string ClientSecret = "B9S_kRLuAie9ToOFLntxIBuLDj-SIA";
+        public static string ClientId = "YourClientIdHere";
+        public static string ClientSecret = "YourClientSecretHere";
         public static string RedirectUri = "http://127.0.0.1:3000/reddit-callback";
     }
 }
