4cba956b2ed1a0b6d314e2995917ea8ac7dbfe79
SimpleBear in developer mode <81253203+itsWindows11@users.noreply.github.com>
2022-08-22 19:10:23 +0200

    Add more pages and functionality

--- Carpeddit/Carpeddit.App/FirstRunPage.xaml.cs
+++ Carpeddit/Carpeddit.App/FirstRunPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Carpeddit.App.Pages.Setup;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Animation;
 
@@ -10,9 +11,36 @@ namespace Carpeddit.App
         {
             InitializeComponent();
 
-            //Loaded += async (s, e) => await new FirstRunDialog().ShowAsync();
-
-            ContentFrame.Navigate(typeof(WelcomePage), null, new SuppressNavigationTransitionInfo());
+            Navigate(true);
         }
+
+        private void Navigate(bool suppressAnimation = false)
+        {
+            switch (App.SViewModel.SetupProgress)
+            {
+                case 0:
+                default:
+                    ContentFrame.Navigate(typeof(WelcomePage), null, suppressAnimation ? new SuppressNavigationTransitionInfo() : new EntranceNavigationTransitionInfo());
+                    break;
+                case 1:
+                    if (App.CurrentAccount != null && App.CurrentAccount.LoggedIn)
+                        ContentFrame.Navigate(typeof(LoginPage), null, suppressAnimation ? new SuppressNavigationTransitionInfo() : new EntranceNavigationTransitionInfo());
+                    else
+                        ContentFrame.Navigate(typeof(NotificationsPage), null, suppressAnimation ? new SuppressNavigationTransitionInfo() : new EntranceNavigationTransitionInfo());
+                    break;
+                case 2:
+                    ContentFrame.Navigate(typeof(NotificationsPage), null, suppressAnimation ? new SuppressNavigationTransitionInfo() : new EntranceNavigationTransitionInfo());
+                    break;
+            }
+        }
+
+        private void OnBackButtonClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            App.SViewModel.SetupProgress--;
+            Navigate();
+        }
+
+        private Visibility GreaterThanZero(int num)
+            => num > 0 ? Visibility.Visible : Visibility.Collapsed;
     }
 }
