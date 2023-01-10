87d310c15c5df4c5b997ba4dd4e4b5fb9ec4d9a6
SimpleBear <itswin11@outlook.com>
2023-01-10 13:18:43 +0200

    Removed old codebase

--- Carpeddit/Carpeddit.Common/Other/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Carpeddit.App.Other
-{
-    public static class Constants
-    {
-        public static string ClientId = "YourClientIdHere";
-        public static string ClientSecret = "YourClientSecretHere";
-        public static string RedirectUri = "http://127.0.0.1:3000/reddit-callback";
-    }
-}
