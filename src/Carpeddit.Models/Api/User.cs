37786ec91a29b466273ae0556b0192688ce2c08d
SimpleBear <itswin11@outlook.com>
2023-01-15 15:00:20 +0200

    Moved API models to separate folder, expanded `User` model

---
+++ src/Carpeddit.Models/Api/User.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Carpeddit.Models
+{
+    public sealed class User
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("over_18")]
+        public bool Over18 { get; set; }
+
+        [JsonPropertyName("is_friend")]
+        public bool IsFriend { get; set; }
+
+        [JsonPropertyName("is_mod")]
+        public bool IsMod { get; set; }
+
+        [JsonPropertyName("link_karma")]
+        public int LinkKarma { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("modhash")]
+        public string ModHash { get; set; }
+
+        [JsonPropertyName("inbox_count")]
+        public int InboxCount { get; set; }
+
+        [JsonPropertyName("comment_karma")]
+        public int CommentKarma { get; set; }
+
+        [JsonPropertyName("awardee_karma")]
+        public int AwardeeKarma { get; set; }
+
+        [JsonPropertyName("total_karma")]
+        public int TotalKarma { get; set; }
+
+        [JsonPropertyName("awarder_karma")]
+        public int AwarderKarma { get; set; }
+
+        [JsonPropertyName("has_mail")]
+        public bool? HasMail { get; set; }
+
+        [JsonPropertyName("has_mod_mail")]
+        public bool? HasModMail { get; set; }
+
+        [JsonPropertyName("has_verified_email")]
+        public bool HasVerifiedEmail { get; set; }
+
+        [JsonPropertyName("has_subscribed_to_premium")]
+        public bool HasSubscribedToPremium { get; set; }
+
+        [JsonPropertyName("has_external_account")]
+        public bool HasExternalAccount { get; set; }
+
+        [JsonPropertyName("is_employee")]
+        public bool IsEmployee { get; set; }
+
+        [JsonPropertyName("subreddit")]
+        public UserSubreddit Subreddit { get; set; }
+
+        [JsonPropertyName("snoovatar_img")]
+        public string SnooavatarImage { get; set; }
+
+        [JsonPropertyName("snoovatar_size")]
+        public int[] SnooavatarSizes { get; set; }
+
+        [JsonPropertyName("gold_expiration")]
+        public object GoldExpiration { get; set; }
+
+        [JsonPropertyName("has_gold_subscription")]
+        public bool HasGoldSubscription { get; set; }
+
+        [JsonPropertyName("is_sponsor")]
+        public bool IsSponsor { get; set; }
+
+        [JsonPropertyName("num_friends")]
+        public int NumFriends { get; set; }
+
+        [JsonPropertyName("can_edit_name")]
+        public bool CanEditName { get; set; }
+
+        [JsonPropertyName("verified")]
+        public bool Verified { get; set; }
+
+        [JsonPropertyName("new_modmail_exists")]
+        public bool NewModmailExists { get; set; }
+
+        [JsonPropertyName("coins")]
+        public int Coins { get; set; }
+
+        [JsonPropertyName("has_paypal_subscription")]
+        public bool HasPaypalSubscription { get; set; }
+
+        [JsonPropertyName("has_stripe_subscription")]
+        public bool HasStripeSubscription { get; set; }
+
+        [JsonPropertyName("can_create_subreddit")]
+        public bool CanCreateSubreddit { get; set; }
+
+        [JsonPropertyName("is_gold")]
+        public bool IsGold { get; set; }
+
+        [JsonPropertyName("suspension_expiration_utc")]
+        public int? SuspensionExpirationUTC { get; set; }
+
+        [JsonPropertyName("is_suspended")]
+        public bool IsSuspended { get; set; }
+
+        [JsonPropertyName("in_redesign_beta")]
+        public bool InRedesignBeta { get; set; }
+
+        [JsonPropertyName("icon_img")]
+        public string IconImage { get; set; }
+
+        [JsonPropertyName("has_mod_mail")]
+        public bool HasModmail { get; set; }
+
+        [JsonPropertyName("pref_nightmode")]
+        public bool NightMode { get; set; }
+
+        [JsonPropertyName("hide_from_robots")]
+        public bool HideFromRobots { get; set; }
+
+        [JsonPropertyName("password_set")]
+        public bool? PasswordSet { get; set; }
+
+        [JsonPropertyName("pref_show_snoovatar")]
+        public bool PrefShowSnooavatar { get; set; }
+
+        [JsonPropertyName("created")]
+        public long Created { get; set; }
+
+        [JsonPropertyName("gold_creddits")]
+        public int GoldCreddits { get; set; }
+
+        [JsonPropertyName("created_utc")]
+        public long CreatedUtc { get; set; }
+
+        [JsonPropertyName("pref_show_twitter")]
+        public bool PrefShowTwitter { get; set; }
+
+        [JsonPropertyName("in_beta")]
+        public bool InBeta { get; set; }
+
+        [JsonPropertyName("accept_followers")]
+        public bool AcceptFollowers { get; set; }
+
+        [JsonPropertyName("has_subscribed")]
+        public bool HasSubscribed { get; set; }
+
+        [JsonPropertyName("linked_identities")]
+        public IEnumerable<string> LinkedIdentities { get; set; }
+    }
+}
---
+++ src/Carpeddit.Models/Api/UserSubreddit.cs
@@ -0,0 +1,128 @@
+﻿using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace Carpeddit.Models
+{
+    public sealed class UserSubreddit
+    {
+        [JsonPropertyName("default_set")]
+        public bool DefaultSet { get; set; }
+
+        [JsonPropertyName("user_is_contributor")]
+        public bool UserIsContributor { get; set; }
+
+        [JsonPropertyName("banner_img")]
+        public string BannerImage { get; set; }
+
+        [JsonPropertyName("restrict_posting")]
+        public bool RestrictPosting { get; set; }
+
+        [JsonPropertyName("user_is_banned")]
+        public bool UserIsBanned { get; set; }
+
+        [JsonPropertyName("free_form_reports")]
+        public bool FreeFormReports { get; set; }
+
+        [JsonPropertyName("community_icon")]
+        public string CommunityIcon { get; set; }
+
+        [JsonPropertyName("show_media")]
+        public bool ShowMedia { get; set; }
+
+        [JsonPropertyName("icon_color")]
+        public string IconColor { get; set; }
+
+        [JsonPropertyName("user_is_muted")]
+        public bool? UserIsMuted { get; set; }
+
+        [JsonPropertyName("display_name")]
+        public string DisplayName { get; set; }
+
+        [JsonPropertyName("header_img")]
+        public string HeaderImage { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("coins")]
+        public int Coins { get; set; }
+
+        [JsonPropertyName("previous_names")]
+        public JsonArray PreviousUsernames { get; set; }
+
+        [JsonPropertyName("over_18")]
+        public bool Over18 { get; set; }
+
+        [JsonPropertyName("icon_size")]
+        public int[] IconSizes { get; set; }
+
+        [JsonPropertyName("primary_color")]
+        public string PrimaryColor { get; set; }
+
+        [JsonPropertyName("icon_img")]
+        public string IconImage { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("header_size")]
+        public int? HeaderSize { get; set; }
+
+        [JsonPropertyName("restrict_commenting")]
+        public bool RestrictCommenting { get; set; }
+
+        [JsonPropertyName("subscribers")]
+        public int Subscribers { get; set; }
+
+        [JsonPropertyName("submit_text_label")]
+        public string SubmitTextLabel { get; set; }
+
+        [JsonPropertyName("is_default_icon")]
+        public string IsDefaultIcon { get; set; }
+
+        [JsonPropertyName("link_flair_position")]
+        public string LinkFlairPosition { get; set; }
+
+        [JsonPropertyName("display_name_prefixed")]
+        public string DisplayNamePrefixed { get; set; }
+
+        [JsonPropertyName("key_color")]
+        public string KeyColor { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("is_default_banner")]
+        public bool IsDefaultBanner { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("quarantine")]
+        public bool Quarantine { get; set; }
+
+        [JsonPropertyName("banner_size")]
+        public int? BannerSize { get; set; }
+
+        [JsonPropertyName("user_is_moderator")]
+        public bool UserIsModerator { get; set; }
+
+        [JsonPropertyName("accept_followers")]
+        public bool AcceptFollowers { get; set; }
+
+        [JsonPropertyName("public_description")]
+        public string PublicDescription { get; set; }
+
+        [JsonPropertyName("link_flair_enabled")]
+        public bool LinkFlairEnabled { get; set; }
+
+        [JsonPropertyName("disable_contributor_requests")]
+        public bool DisableContributorRequests { get; set; }
+
+        [JsonPropertyName("subreddit_type")]
+        public string SubredditType { get; set; }
+
+        [JsonPropertyName("user_is_subscriber")]
+        public bool UserIsSubscriber { get; set; }
+    }
+}
